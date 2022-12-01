aac4f3e9993e5688099ae8700a872e382990dbb6
Andrew Lin <andrewlin16@gmail.com>
2022-12-01 02:47:48 -0600

    Add solution for 1a

---
+++ src/1a.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn main() {
+    let mut max = 0;
+    let mut sum = 0;
+
+    for line in io::stdin().lines() {
+        let line = line.unwrap();
+
+        if line.len() == 0 {
+            if sum > max {
+                max = sum;
+            }
+            sum = 0;
+        } else {
+            let cal: u32 = line.trim().parse().expect("invalid value");
+            sum += cal;
+        }
+    }
+
+    if sum > max {
+        max = sum;
+    }
+    println!("{}", max);
+}
