7c2182df1d148c0ed92e748b167d50cf59fdf37a
Andrew Lin <andrewlin16@gmail.com>
2022-12-06 02:03:41 -0600

    Add solution to 6a

---
+++ src/6a.rs
@@ -0,0 +1,53 @@
+use std::collections::HashMap;
+use std::io;
+
+fn inc(map: &mut HashMap<u8, u8>, val: u8) {
+    match map.get(&val) {
+        Some(v) => {
+            map.insert(val, v + 1);
+        }
+        None => {
+            map.insert(val, 1);
+        }
+    }
+}
+
+fn dec(map: &mut HashMap<u8, u8>, val: u8) {
+    match map.get(&val) {
+        Some(1) => {
+            map.remove(&val);
+        }
+        Some(v) => {
+            map.insert(val, v - 1);
+        }
+        None => {
+            panic!("val that is being decremented should be in map");
+        }
+    }
+}
+
+fn main() {
+    let mut line = String::new();
+    let _ = io::stdin().read_line(&mut line);
+
+    let mut map = HashMap::new();
+    for b in line.bytes().take(4) {
+        inc(&mut map, b);
+    }
+
+    if map.len() == 4 {
+        println!("4");
+        return;
+    }
+
+    let bytes = line.as_bytes();
+    for i in 4..bytes.len() - 1 {
+        inc(&mut map, bytes[i]);
+        dec(&mut map, bytes[i - 4]);
+
+        if map.len() == 4 {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+}
