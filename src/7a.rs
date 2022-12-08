6648d3ee472b5a702e151c901f5ead188230e53e
Andrew Lin <andrewlin16@gmail.com>
2022-12-08 03:35:11 -0600

    Add solution to 7a
    This one require a LOT of Rust learnings, hence why it took so long.
    Also, I was getting stuck on how to use the smart pointer stuff as well
    as a bunch of other compile errors and even some weird inconsistent
    answers on subsequent runs which somehow magically fixed itself. This
    almost made me want to ragequit Rust @_@
    
    In particular, trying to figure out smart pointers and borrowing/mutable
    borrowing was a major PITA when creating the tree structure. I actually
    gave up the original attempt to make a "real" tree with parent pointers
    and went with the simpler arena approach (which I also learned about for
    this problem).
    
    Also, my takeaway is to just compile something and then try to do
    whatever rustc suggests. That, or just stick & and * around all the Rc's
    or RefCells. They don't sound like good takeaways though.

---
+++ src/7a.rs
@@ -0,0 +1,145 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::io;
+use std::rc::Rc;
+use std::vec::Vec;
+
+struct Filesystem {
+    entities: Vec<Rc<RefCell<Entity>>>,
+}
+
+impl Filesystem {
+    fn new() -> Filesystem {
+        let root = Entity::Dir(Directory::new(0));
+        return Filesystem {
+            entities: vec![Rc::new(RefCell::new(root))],
+        };
+    }
+
+    fn add_entity(&mut self, entity: Rc<RefCell<Entity>>) -> usize {
+        let index = self.entities.len();
+        self.entities.push(Rc::clone(&entity));
+        return index;
+    }
+
+    fn get(&self, index: usize) -> Rc<RefCell<Entity>> {
+        Rc::clone(&self.entities[index])
+    }
+
+    fn dirs(&self) -> impl Iterator<Item = Rc<RefCell<Directory>>> + '_ {
+        return self
+            .entities
+            .iter()
+            .filter(|e| matches!(&*e.borrow(), Entity::Dir(_)))
+            .map(|e| Rc::clone(&e.borrow().as_dir()));
+    }
+}
+
+enum Entity {
+    File(u64),
+    Dir(Rc<RefCell<Directory>>),
+}
+
+impl Entity {
+    fn as_dir(&self) -> Rc<RefCell<Directory>> {
+        match self {
+            Entity::File(_) => panic!("entity should be Dir"),
+            Entity::Dir(d) => Rc::clone(&d),
+        }
+    }
+}
+
+struct Directory {
+    parent: usize,
+    contents: HashMap<String, usize>,
+}
+
+impl Directory {
+    fn new(parent: usize) -> Rc<RefCell<Directory>> {
+        return Rc::new(RefCell::new(Directory {
+            parent: parent,
+            contents: HashMap::new(),
+        }));
+    }
+
+    fn insert(&mut self, name: String, index: usize) {
+        self.contents.insert(name, index);
+    }
+
+    fn get(&self, name: String) -> Option<usize> {
+        self.contents.get(&name).map(|v| *v)
+    }
+
+    fn size(&self, fs: &Filesystem) -> u64 {
+        self.contents.values().fold(0, |acc, i| {
+            return acc
+                + match &*fs.get(*i).borrow() {
+                    Entity::File(s) => *s,
+                    Entity::Dir(d) => d.borrow().size(fs),
+                };
+        })
+    }
+}
+
+fn main() {
+    let mut fs = Filesystem::new();
+    let mut cd = 0;
+
+    for line in io::stdin().lines() {
+        let line = line.unwrap();
+
+        if line.chars().nth(0).unwrap() == '$' {
+            // Parse out shell command.
+            let cmd = &line[2..];
+            if cmd.starts_with("cd") {
+                let target = &cmd[3..];
+                if target == "/" {
+                    // Set current directory to root FS directory.
+                    cd = 0;
+                } else if target == ".." {
+                    // Go up one level via current directory's parent.
+                    let dir = fs.get(cd).borrow().as_dir();
+                    cd = dir.borrow().parent;
+                } else {
+                    // Go down one level by getting named directory from contents.
+                    let dir = fs.get(cd).borrow().as_dir();
+                    cd = dir
+                        .borrow()
+                        .get(target.to_string())
+                        .expect("cd target should exist");
+                }
+            }
+            // If command is not cd, then it must be ls, which will then go to
+            // the else case below.
+        } else {
+            // Add entry from ls into current directory.
+            let dir = fs.get(cd).borrow_mut().as_dir();
+
+            let mut entry_split = line.splitn(2, ' ');
+            let meta = entry_split.next().expect("entry should have dir or size");
+            let name = entry_split
+                .next()
+                .expect("entry should have name")
+                .to_string();
+
+            if meta == "dir" {
+                let ent = Rc::new(RefCell::new(Entity::Dir(Directory::new(cd))));
+                let new_dir = fs.add_entity(ent);
+                dir.borrow_mut().insert(name, new_dir);
+            } else {
+                let ent = Rc::new(RefCell::new(Entity::File(
+                    meta.parse().expect("size field should be a number"),
+                )));
+                let file = fs.add_entity(ent);
+                dir.borrow_mut().insert(name, file);
+            }
+        }
+    }
+
+    let size_sum: u64 = fs
+        .dirs()
+        .map(|d| d.borrow().size(&fs))
+        .filter(|&s| s <= 100000)
+        .sum();
+    println!("{}", size_sum);
+}
