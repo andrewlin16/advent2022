0299e4ced76edd592a958ca9c4e0958cbe219f8b
Andrew Lin <andrewlin16@gmail.com>
2022-12-11 02:32:54 -0600

    Add solution to 11b
    Note that the solution here exploits a property of the input I saw: each
    monkey's divisibility test uses a distinct prime number. Because of
    that, this solution (ab)uses modular arithmetic to store modulos of the
    worry levels instead of the actual worry level to avoid integer overflow
    while still being able to correctly apply the divisibility test for each
    item.
    
    It was that, or pull in some bignum/arbitrary precision integer crate,
    but I'm aiming to stay within the language's standard library, and I
    wasn't about to go implement bignums. Also, it's not often that I get to
    flex my number theory knowledge :P
    
    So, maybe this approach is technically cheating (in that you're not
    supposed to assume the divisibility tests all use coprime modulos)...
    but hey, it works for me.

---
+++ src/11b.rs
@@ -0,0 +1,165 @@
+use std::cell::RefCell;
+use std::io;
+use std::rc::Rc;
+use std::vec::Vec;
+
+enum Operator {
+    Add,
+    Multiply,
+}
+
+enum Operand {
+    Old,
+    Number(u64),
+}
+
+struct Operation {
+    operator: Operator,
+    operand: Operand,
+}
+
+impl Operation {
+    fn from(s: String) -> Operation {
+        Operation {
+            operator: match s.chars().nth(0).expect("operation should have operator") {
+                '+' => Operator::Add,
+                '*' => Operator::Multiply,
+                _ => panic!("operator in operation should be + or *"),
+            },
+            operand: {
+                let s = &s[2..];
+                if s == "old" {
+                    Operand::Old
+                } else {
+                    Operand::Number(
+                        s.parse()
+                            .expect("operand in operation should be number if not 'old'"),
+                    )
+                }
+            },
+        }
+    }
+
+    fn apply(&self, val: u64, modulo: u64) -> u64 {
+        let o = match self.operand {
+            Operand::Old => val,
+            Operand::Number(n) => n,
+        };
+
+        return match self.operator {
+            Operator::Add => (val + o) % modulo,
+            Operator::Multiply => val * o % modulo,
+        };
+    }
+}
+
+struct Monkey {
+    items: Rc<RefCell<Vec<u64>>>,
+    operation: Operation,
+    test: u64,
+    true_target: usize,
+    false_target: usize,
+    inspects: RefCell<usize>,
+}
+
+fn main() {
+    let mut line = String::new();
+    let mut monkeys: Vec<Rc<RefCell<Monkey>>> = Vec::new();
+    // NOTE: This approach exploits a property of the input that isn't
+    // explicitly listed in the problem - each monkey's divisibily test is a
+    // distinct prime number, hence we can store modulos of each worry level
+    // instead of the raw worry level to avoid integer overflow.
+    let mut modulo = 1;
+
+    loop {
+        // Read "Monkey n:" line.
+        let _ = io::stdin().read_line(&mut line);
+
+        // Read and parse "Starting items: ..." line.
+        line.clear();
+        let _ = io::stdin().read_line(&mut line);
+        let items: Vec<u64> = line[18..]
+            .split(", ")
+            .map(|s| s.trim().parse().expect("item should be numeric"))
+            .collect();
+
+        // Read and parse "Operation: ..." line.
+        line.clear();
+        let _ = io::stdin().read_line(&mut line);
+        let operation = Operation::from(line[23..].trim().to_string());
+
+        // Read and parse "Test: ..." line.
+        line.clear();
+        let _ = io::stdin().read_line(&mut line);
+        let test = line[21..]
+            .trim()
+            .parse()
+            .expect("test divisible by number should be numeric");
+
+        // Read and parse "If true: ..." line.
+        line.clear();
+        let _ = io::stdin().read_line(&mut line);
+        let true_target = line[29..]
+            .trim()
+            .parse()
+            .expect("If true target should be numeric");
+
+        // Read and parse "If false: ..." line.
+        line.clear();
+        let _ = io::stdin().read_line(&mut line);
+        let false_target = line[30..]
+            .trim()
+            .parse()
+            .expect("If true target should be numeric");
+
+        // Create Monkey and add to vec.
+        monkeys.push(Rc::new(RefCell::new(Monkey {
+            items: Rc::new(RefCell::new(items)),
+            operation: operation,
+            test: test,
+            true_target: true_target,
+            false_target: false_target,
+            inspects: RefCell::new(0),
+        })));
+        modulo *= test;
+
+        // Read blank line/check for end of input.
+        let cnt = io::stdin().read_line(&mut line);
+        if cnt.unwrap() == 0 {
+            // Reached EOF, go to processing stage.
+            break;
+        }
+    }
+
+    eprintln!("{}", modulo);
+
+    // Run rounds on monkeys.
+    for _ in 0..10000 {
+        for c in &monkeys {
+            let m = c.borrow_mut();
+            let mut items = m.items.borrow_mut();
+            *m.inspects.borrow_mut() += items.len();
+
+            let op = &m.operation;
+            let test = &m.test;
+            let true_target = &m.true_target;
+            let false_target = &m.false_target;
+
+            for v in items.drain(..) {
+                let new_val = op.apply(v, modulo);
+                let target_num = if new_val % test == 0 {
+                    true_target
+                } else {
+                    false_target
+                };
+                let target = monkeys[*target_num].borrow_mut();
+                target.items.borrow_mut().push(new_val);
+            }
+        }
+    }
+
+    // Find top 2 active monkeys to calculate monkey business.
+    monkeys.select_nth_unstable_by_key(1, |m| -(*m.borrow().inspects.borrow() as i64));
+    let business = *monkeys[0].borrow().inspects.borrow() * *monkeys[1].borrow().inspects.borrow();
+    println!("{}", business);
+}
